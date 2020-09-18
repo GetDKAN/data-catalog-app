d2111957b88e0d94628a2e403145b7fdab935688
Daniel Gading <daniel.gading@civicactions.com>
2020-09-18 11:12:11 -0400

    Fix publisher test

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -106,7 +106,7 @@ context('Search', () => {
     cy.stubSearchResults('/search');
     cy.get('.dc-search-results-message').contains('10 datasets found');
     cy.get(searchList).children().its('length').should('eq', 10);
-    cy.wait(5)
+    cy.wait(500)
     cy.findByText('Advisory Council for Infectious Disease (2)').click();
     cy.get('.dc-search-results-message').contains('2 datasets found in Publishers: Advisory Council for Infectious Disease');
     cy.get(searchList).children().its('length').should('eq', 2);
