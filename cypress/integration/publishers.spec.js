c7fdd4120a9013aaacac14ef0a5de9c933ca9401
Janette Day <janette.day@civicactions.com>
2020-09-25 11:17:56 -0500

    add test for org link

--- cypress/integration/publishers.spec.js
+++ cypress/integration/publishers.spec.js
@@ -14,5 +14,10 @@ context('Publishers', () => {
       cy.findByAltText('Committee on International Affairs');
       cy.findByRole('heading', { name: 'Committee on International Affairs' });
     })
+    // Enable when components are updated.
+    it.skip('Links should take me to the search page', () => {
+      cy.findByRole('link', { name: 'Publishers' }).click({ force: true });
+      cy.get('.dc-publisher-list  > :nth-child(1) > a').should('have.attr', 'href').and('eq', '../search?publisher__name=State Economic Council');
+    })
 
   });
