53f17815d5e7540a58dc30b29c5db6a95b6f0b32
Janette Day <janette.day@civicactions.com>
2020-12-17 07:58:31 -0600

    Update Resource to handle non-csv and accessUrl properties (#46)

--- src/components/Resource/index.jsx
+++ src/components/Resource/index.jsx
@@ -7,8 +7,14 @@ import {
 } from "@civicactions/data-catalog-components";
 
 const ResourceTemplate = ({ resource }) => {
-  const format = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('format') ? resource.data.format : 'unknown';
+  const type = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('mediaType') ? resource.data.mediaType.split("/") : '';
+  const backup = type ? type[1] : 'data';
+  const format = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('format') ? resource.data.format : backup;
+
+  const downloadURL = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('downloadURL') ? resource.data.downloadURL : '';
+  const accessURL = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('accessURL') ? resource.data.accessURL : downloadURL;
   const rootURL = `${process.env.REACT_APP_ROOT_URL}/`;
+
   return (
     <div id={`resource_${resource.identifier}`}>
       {format.toLowerCase() === 'csv'
@@ -23,7 +29,7 @@ const ResourceTemplate = ({ resource }) => {
               title={resource.data.title}
               label={resource.data.downloadURL}
               format={format}
-              downloadURL={resource.data.downloadURL}
+              downloadURL={downloadURL ? downloadURL : accessURL}
             />
             <DataTableHeader />
             <DataTable />
