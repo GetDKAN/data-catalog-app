8d975b745398c7e7c0345a0bacb20d8aed411b4f
Dharizza Espinach B <dharizza@gmail.com>
2021-10-02 18:12:51 -0600

    Make publishers list to be dynamic instead of using assets file.

--- src/templates/publishers/index.jsx
+++ src/templates/publishers/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import { PublisherList } from "@civicactions/data-catalog-components";
 import config from "../../assets/config";
 import Layout from '../../components/Layout';
-import orgs from "../../assets/publishers";
 
 const Publishers = () => {
+  const { t } = useTranslation('publishers');
+  const [orgs, setOrgs] = React.useState(null);
+  const [publishers, setPublishers] = React.useState([])
+
+  React.useEffect(() => {
+    async function getOrgs() {
+      const { data } = await axios.get(`${process.env.REACT_APP_ROOT_URL}/metastore/schemas/publisher/items`)
+      setOrgs(data);
+    }
+    if (orgs === null) {
+      getOrgs();
+    }
+    if (orgs) {
+      const items = [];
+      orgs.forEach(element => {
+        items.push(element.data);
+      });
+      setPublishers(items);
+    }
+  }, [orgs])
+
   return (
     <Layout title="Dataset Publishers">
     <div className={`dc-page ${config.container}`}>
@@ -20,7 +41,7 @@ const Publishers = () => {
           publishing data to the same site.
         </p>
 
-        <PublisherList items = {orgs} />
+        <PublisherList items = {publishers} />
 
       </div>
     </div>
