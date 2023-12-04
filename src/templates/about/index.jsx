5ae9994f5a725da63d9135991b0b7c2ab4f71e03
Bethany Dunfield <brdunfield@gmail.com>
2023-12-04 08:48:56 -0700

    Add app version and data-catalog-components version to about page (#130)

--- src/templates/about/index.jsx
+++ src/templates/about/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Announcement } from "@civicactions/data-catalog-components";
 import Layout from '../../components/Layout';
 import config from "../../assets/config";
+import { version, dependencies } from '../../../package.json';
 
-const About = ({ path }) => (
+const About = () => (
     <Layout title="About">
         <div className={`dc-page ${config.container}`}>
             <h1>About this site</h1>
             <div className="dc-page-content row">
-                <div class="col-md-9 col-sm-12">
+                <div className="col-md-9 col-sm-12">
                     <p>This is the default state of the DKAN data catalog.</p>
                     <p>
                     This tool helps create open data catalogs using React and other
@@ -20,6 +21,13 @@ const About = ({ path }) => (
                     <p>Update this about page before publishing.</p>
                     </Announcement>
                 </div>
+            </div>
+            <div className="dc-page-content row">
+                <h2>App version:</h2>
+                <div className="col-12">
+                    <p>data-catalog-app: {version}</p>
+                    <p>data-catalog-components: {dependencies["@civicactions/data-catalog-components"]}</p>
+                </div>  
             </div>
         </div>
     </Layout>
