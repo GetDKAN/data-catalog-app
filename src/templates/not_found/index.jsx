b48ad0c3354be144a08cbc193a659961217a2f7e
Daniel Gading <dgading@proton.me>
2024-02-23 14:43:07 -0500

    wrap notfound template with Layout component

--- src/templates/not_found/index.jsx
+++ src/templates/not_found/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Layout from '../../components/Layout';
 
 const NotFound = () => {
   return (
-    <h1>Page not found.</h1>
+    <Layout title="Dataset Publishers">
+      <div class="dc-page container">
+        <h1>Page not found.</h1>
+        <p>We're sorry, we can't find the page you're looking for.</p>
+      </div>
+    </Layout>
   );
 }
 
